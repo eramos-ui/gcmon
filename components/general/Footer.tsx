825f7e3aa4aa89e74b2b6276a078a5c434d31b7b
eramos-ui <eramosarellano@gmail.com>
2025-07-20 12:40:01 -0400

    0.2-kappa

--- components/general/Footer.tsx
+++ components/general/Footer.tsx
@@ -1,4 +1,4 @@
-const version='0.2-iota';
+const version='0.2-kappa';
 export const Footer = () => (
     <footer className="text-center text-gray-400 text-sm py-2 border-t">
       © {new Date().getFullYear()}  version {version}
--- lib/db.ts
+++ lib/db.ts
@@ -4,10 +4,10 @@ import mongoose from 'mongoose';
 import { MongoClient } from 'mongodb';
 
 
-//const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://jonnyheiss:qoynVk4Vn430yEd9@clustergc.4g4jumk.mongodb.net/GastosComunes?retryWrites=true&w=majority';
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/GastosComunes?replicaSet=rs0';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://jonnyheiss:qoynVk4Vn430yEd9@clustergc.4g4jumk.mongodb.net/GastosComunes?retryWrites=true&w=majority';
+//const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/GastosComunes?replicaSet=rs0';
 
-console.log("En dn MONGODB_URI:", MONGODB_URI);
+console.log("En db MONGODB_URI:", MONGODB_URI);
 if (!MONGODB_URI) {
   throw new Error('❌ MONGODB_URI no está definido en las variables de entorno.');
 }
